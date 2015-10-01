a2bf8cd2cb5114ebab6e8a8485161357126aacf1
Javier Murillo <jamkoons@gmail.com>
2015-09-30 17:48:04 -0700

    Create AtlasStyle.h

---
+++ ATLAS/AtlasStyle.h
@@ -0,0 +1,22 @@
+//
+//   @file    AtlasStyle.h         
+//   
+//            ATLAS Style, based on a style file from BaBar
+//
+//
+//   @author M.Sutton
+// 
+//   Copyright (C) 2010 Atlas Collaboration
+//
+//   $Id: AtlasStyle.h 346 2013-05-07 11:32:13Z achishol $
+
+#ifndef  __ATLASSTYLE_H
+#define __ATLASSTYLE_H
+
+#include "TStyle.h"
+
+void SetAtlasStyle();
+
+TStyle* AtlasStyle(); 
+
+#endif // __ATLASSTYLE_H
